4a99ca70f270c9ef24c707033f7e8dcd91567d11
Eugene2001-boom <leekiplee2001@gmail.com>
2020-08-05 12:44:50 +0300

    add initial page structure

--- js/scripts.js
+++ js/scripts.js
@@ -13,15 +13,16 @@ $(document).ready(function() {
       $(".details3").toggle(500);
     });
     $(".fourth").click(function() {
-      $(".illustration3").toggle(500);
-      $(".details3").toggle(500);
+      $(".illustration4").toggle(500);
+      $(".details4").toggle(500);
     });
     $(".fifth").click(function() {
-      $(".illustration3").toggle(500);
-      $(".details3").toggle(500);
+      $(".illustration5").toggle(500);
+      $(".details5").toggle(500);
     });
     $(".sixth").click(function() {
-      $(".illustration3").toggle(500);
-      $(".details3").toggle(500);
+      $(".illustration6").toggle(500);
+      $(".details6").toggle(500);
     });
-  }  
+  });
+    
